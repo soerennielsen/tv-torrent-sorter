a3029568b8d8bf937653e22875289e0520534592
John Deerhake <jdeerhake@gmail.com>
2014-02-09 23:48:48 -0800

    Wrap in try/catch

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 #!/usr/bin/env node
 
-var conf = require( './settings' ),
-  show = require( './src/show' ),
-  notify = require( './src/notify' );
+var notify = require( './src/notify' );
+
+try {
+  var conf = require( './settings' ),
+    show = require( './src/show' );
 
 
-function save( obj, key ) {
-  return function( val ) {
-    obj[ key ] = val;
-    return val;
-  };
+  function save( obj, key ) {
+    return function( val ) {
+      obj[ key ] = val;
+      return val;
+    };
+  }
+
+  var state = {};
+
+  require( './src/subdirs' )( conf.tvShowsDir )
+    .then( save( show, 'shows' ) )
+    .then( require( './src/torrent_info' ) )
+    .then( save( state, 'torrent' ) )
+    .then( require( './src/finder' ) )
+    .then( require( './src/sort_files') )
+    .then( require( './src/matcher') )
+    .then( function( files ) {
+      state.files = files;
+      return state;
+    } )
+    .then( require( './src/make_dirs' ) )
+    .then( require( './src/move_files' ) )
+    .done( notify.success, function( err ) {
+      notify.err( err, state );
+    });
+} catch( e ) {
+  notify.err( e, state );
 }
-
-var state = {};
-
-require( './src/subdirs' )( conf.tvShowsDir )
-  .then( save( show, 'shows' ) )
-  .then( require( './src/torrent_info' ) )
-  .then( save( state, 'torrent' ) )
-  .then( require( './src/finder' ) )
-  .then( require( './src/sort_files') )
-  .then( require( './src/matcher') )
-  .then( function( files ) {
-    state.files = files;
-    return state;
-  } )
-  .then( require( './src/make_dirs' ) )
-  .then( require( './src/move_files' ) )
-  .done( notify.success, function( err ) {
-    notify.err( err, state );
-  });
