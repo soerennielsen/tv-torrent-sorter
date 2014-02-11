b0822348337dc657eb74076da8e5b29f967118d4
John Deerhake <jdeerhake@gmail.com>
2014-02-10 20:47:58 -0800

    Add ext validation

--- src/filter_files.js
+++ src/filter_files.js
@@ -1,16 +1,34 @@
 var conf = require( '../settings' ),
   helpers = require( './helpers' );
 
-module.exports = function( files ) {
-  var minSize = helpers.strToBytes( conf.minFileSize );
+var VIDEO_EXT = conf.validVideoExt.map(function( ext ) { return ext.toLowerCase(); }),
+ minSize = helpers.strToBytes( conf.minFileSize );
 
-  var largeFiles = files.filter(function( file ) {
-    return file.size > minSize;
-  });
 
-  if( largeFiles.length === 0 ) {
-    throw 'No files are big enough';
+function isMinSize( file ) {
+  return file.size > minSize;
+}
+
+function isVideoExt( file ) {
+  var ext = file.filename.match( /\.([a-zA-Z0-9]{2,4})$/ );
+
+  if( ext && ext.length === 2 ) {
+    return VIDEO_EXT.indexOf( ext[1].toLowerCase() ) !== -1;
   }
 
-  return largeFiles;
+  return false;
+}
+
+
+module.exports = function( files ) {
+
+  var vidFiles = files
+                    .filter( isMinSize )
+                    .filter( isVideoExt );
+
+  if( vidFiles.length === 0 ) {
+    throw 'No files are big enough or have a video extension';
+  }
+
+  return vidFiles;
 };
